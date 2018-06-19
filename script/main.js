90722e7df7ddd91ac31cfb27045c8333ed008b74
Ben Thomas <benjeh32@gmail.com>
2018-06-19 21:39:29 +0100

    Pretty printed the active time.

--- script/main.js
+++ script/main.js
@@ -6,7 +6,7 @@ var refreshMillis = 1000;
  * Update the game state from server.
  */
 function updateGameState() {
-	// GET game state from server
+    // GET game state from server
     gameState = {
         clue: "🦇👨",
         activeFrom: 1528928914066,
@@ -30,21 +30,58 @@ function updateGameState() {
 }
 
 /**
+ * Convert milliseconds as days, hours, minutes and seconds.
+ * 
+ * @argument milliseconds Milliseconds in the time period to convert.
+ * @returns The time period expressed in the format: 'D days, H hours, M minutes, S seconds'.
+ */
+function convertMilliseconds(milliseconds) {
+
+    // Convert each part
+    var seconds = parseInt((milliseconds / 1000) % 60);
+    var minutes = parseInt((milliseconds / (1000 * 60)) % 60)
+    var hours = parseInt((milliseconds / (1000 * 60 * 60)) % 24);
+    var days = parseInt((milliseconds / (1000 * 60 * 60 * 24)));
+
+    var convertedValue = '';
+
+    // Only include days if at least one day
+    if (days > 0) {
+        convertedValue = convertedValue + days + (days == 1 ? ' day, ' : ' days, ');
+    }
+
+    // Include hours if not the first element, otherwise only include hours if at least one hour
+    if (convertedValue || hours > 0) {
+        convertedValue = convertedValue + hours + (hours == 1 ? ' hour, ' : ' hours, ');
+    }
+
+    // Include minutes if not the first element, otherwise only include minutes if at least one minute
+    if (convertedValue || minutes > 0) {
+        convertedValue = convertedValue + minutes + (minutes == 1 ? ' minute, ' : ' minutes, ');
+    }
+
+    // Always include seconds
+    convertedValue = convertedValue + seconds + (seconds == 1 ? ' second' : ' seconds');
+
+    return convertedValue;
+}
+
+/**
  * Render the game.
  */
 function renderGame() {
 
-	// Update game
-	updateGameState();
-	
-	// Clear previous display
-	$("#clue span").remove();
-	$("#active-time span").remove();
-	$("#previous-games tbody").empty();
+    // Update game
+    updateGameState();
+
+    // Clear previous display
+    $("#clue span").remove();
+    $("#active-time span").remove();
+    $("#previous-games tbody").empty();
 
     // Display current game
     $("#clue").append("<span>" + gameState.clue + "</span>");
-    $("#active-time").append("<span>" + (new Date().getTime() - gameState.activeFrom) + "ms" + "</span>");
+    $("#active-time").append("<span>" + convertMilliseconds((new Date().getTime() - gameState.activeFrom)) + "</span>");
 
     // Display previous games
     gameState.previousGames.forEach(previousGame => {
@@ -52,7 +89,7 @@ function renderGame() {
         $("#previous-games tbody").append("<td>" + previousGame.clue + "</td>");
         $("#previous-games tbody").append("<td>" + previousGame.filmName + "</td>");
         $("#previous-games tbody").append("<td>" + previousGame.solvedBy + "</td>");
-        $("#previous-games tbody").append("<td>" + (previousGame.activeTo - previousGame.activeFrom) + "ms</td>");
+        $("#previous-games tbody").append("<td>" + convertMilliseconds(previousGame.activeTo - previousGame.activeFrom) + "</td>");
         $("#previous-games tbody").append("</tr>");
     });
 }
@@ -61,11 +98,11 @@ function renderGame() {
  * Load the game.
  */
 function loadGame() {
-	// Initial render
-	renderGame();
-	
-	// Render the game on an interval
-	setInterval(renderGame, refreshMillis);	
+    // Initial render
+    renderGame();
+
+    // Render the game on an interval
+    setInterval(renderGame, refreshMillis);
 }
 
 // Main
